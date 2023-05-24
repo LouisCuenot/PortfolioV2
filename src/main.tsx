c5fa0d0d4fe4d972a62845947d9c42e03f5664b7
LouisCuenot <70881127+LouisCuenot@users.noreply.github.com>
2023-05-24 14:21:19 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App from './App';
+import './fonts/Humane-SemiBold.ttf'
+import './fonts/Durer.otf'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+
+
+
+
+root.render(
+    <App />
+);
+
+
